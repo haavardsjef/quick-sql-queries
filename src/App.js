666138ba774c2145e5fa40ade0635224e0c186b0
Håvard Hjelmeseth <haavahje@stud.ntnu.no>
2020-05-07 17:56:20 +0200

    Added functionality toi generate SQL queries for generating tables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { useState } from "react";
 
 function App() {
   const [numCols, setNumCols] = useState(1);
+
+  function generateTableQuery() {
+    const tableName = document.getElementById("table-name").value;
+    var textanswer = "CREATE TABLE " + tableName + " (\n";
+
+    for (let j = 0; j < numCols; j++) {
+      const name = document.getElementById(j + "name").value;
+      const type = document.getElementById(j + "type").value;
+      textanswer += "\t" + name + " " + type;
+      if (j !== numCols - 1) {
+        textanswer += ",";
+      }
+      textanswer += "\n";
+    }
+    textanswer += ");";
+
+    const textField = document.getElementById("generate-table-sql");
+    textField.value = textanswer;
+  }
+
   return (
     <div>
       <div className="container mt-5">
@@ -10,15 +30,21 @@ function App() {
           <div className="form-row">
             <div className="col-12 col-md-6">
               <input
+                id="table-name"
                 type="text"
                 className="form-control"
                 placeholder="Table Name"
+                autoComplete="off"
               />
             </div>
           </div>
           <Columns numCols={numCols} />
         </form>
-        <button type="button" className="btn btn-primary mt-1">
+        <button
+          type="button"
+          className="btn btn-primary mt-1"
+          onClick={() => generateTableQuery()}
+        >
           Generate SQL query
         </button>
         <button
@@ -30,8 +56,7 @@ function App() {
         </button>
         <textarea
           className="form-control mt-5"
-          name=""
-          id=""
+          id="generate-table-sql"
           cols="30"
           rows="10"
         ></textarea>
@@ -47,13 +72,15 @@ function Columns({ numCols }) {
       <div className="form-row mt-1">
         <div className="col">
           <input
+            id={i + "name"}
             type="text"
             className="form-control"
             placeholder="Column name"
+            autoComplete="off"
           />
         </div>
         <div className="col">
-          <select id="inputState" className="form-control">
+          <select id={i + "type"} className="form-control">
             <option>int</option>
             <option>varchar(255)</option>
           </select>
